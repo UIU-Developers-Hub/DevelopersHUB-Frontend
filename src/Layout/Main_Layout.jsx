37da7c514c0475279506611aee69c7422baa4784
Md-Sifat-code <codewithsifat4@gmail.com>
2025-01-02 00:32:35 +0600

    initial setup done

---
+++ src/Layout/Main_Layout.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Outlet } from "react-router-dom";
+import Navbar from "../Fixed_component/Navbar";
+import Footer from "../Fixed_component/Footer";
+
+export default function Main_Layout() {
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+}
